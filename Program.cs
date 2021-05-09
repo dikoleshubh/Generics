cebf9a44c50c063946f39565f8164e8bd5b594f7
dikoleshubh <79848187+dikoleshubh@users.noreply.github.com>
2021-05-09 23:21:09 +0530

    GENERICS

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace generics
         static void Main(string[] args)
         {
             Console.WriteLine("Generics");
+            
+            Console.WriteLine(MaximumNumbers.MaxNumber(64, 53, 32));
+            Console.WriteLine(MaximumNumbers.MaxNumber(43, 443, 32));
+            Console.WriteLine(MaximumNumbers.MaxtNumber(33, 53, 93));
+
 
         }
     }
