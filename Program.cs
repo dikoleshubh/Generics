48abd4ca2825592cfce5a21ad185894612878aef
dikoleshubh <dikole.shubh@gmail.com>
2021-05-09 15:11:35 +0530

    Welcome to generics

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generics");
+
+        }
+    }
+}
