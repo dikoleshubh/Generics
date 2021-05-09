27b34ef8758cf8374649dd5f3a538f83f1d41016
dikoleshubh <79848187+dikoleshubh@users.noreply.github.com>
2021-05-09 23:24:26 +0530

    Maximum number test- Float

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace generics
         static void Main(string[] args)
         {
             Console.WriteLine("Generics");
+           Console.WriteLine(MaximumNumbers.MaxFloatNumber(64.09f, 53.0f, 32.6f);
+           Console.WriteLine(MaximumNumbers.MaxFloatNumber(7.6f, 66.5f, 3.0f));
+           Console.WriteLine(MaximumNumbers.MaxFloatNumber(7.46f, 32.09f, 64.09f));
 
         }
     }
