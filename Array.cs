31db8d3a5a919f106971666cab633e0357029d77
dikoleshubh <dikole.shubh@gmail.com>
2021-05-10 00:04:16 +0530

    Multiple paremeters

---
+++ Array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace generics
+{
+    public class PrintArray
+    {
+        public static void ToPrintInteger(int[] arr)
+        {
+            foreach (int element in arr)
+            {
+                Console.Write(element + "\t");
+            }
+        }
+        public static void ToPrintDouble(double[] arr)
+        {
+            foreach (double element in arr)
+            {
+                Console.Write(element + "\t");
+            }
+        }
+
+        public static void ToPrintCharacter(char[] arr)
+        {
+            foreach (char element in arr)
+            {
+                Console.Write(element + "\t");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace generics
 {
@@ -6,9 +10,24 @@ namespace generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Generics");
-            string INPUT = MaxNumbers.StringMax("NIFTY", "NASDAQ", "SENSEX");
-            Console.Write(INPUT);
+            //Console.WriteLine("Generics");
+            //string INPUT = MaxNumbers.StringMax("NIFTY", "NASDAQ", "SENSEX");
+            //Console.Write(INPUT);
+
+            int[] Output = { 11, 22, 50, 60, 90 };
+            Generic<int> genericInt = new Generic<int>(Output);//create object using int data type
+            genericInt.PrintValue();
+            Console.WriteLine();
+
+            double[] Output1 = { 11.2, 22.5, 50.45, 55.92 };
+            Generic<double> genericdouble = new Generic<double>(Output1);
+            genericdouble.PrintValue();
+            Console.WriteLine( );
+            //String array
+            string[] stringArray = { "11s1", "222", "333", "55", "913213" };
+            Generic<string> genericString = new Generic<string>(stringArray);
+            genericString.PrintValue();
+            Console.Read();
 
         }
     }
